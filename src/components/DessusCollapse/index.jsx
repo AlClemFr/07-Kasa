1a1ddb6821ea2c94908f0b224677b710da9e7e6a
AlClemFr <aclemfr@gmail.com>
2023-03-14 18:20:22 +0100

    2022 03 j14 18h20 - Page Logement - mise en place foto et nom a partir DataBase

--- src/components/DessusCollapse/index.jsx
+++ src/components/DessusCollapse/index.jsx
@@ -8,6 +8,8 @@ import '../../20_styles/DessusCollapse.css'
 //v- images
 import EtoileGrise from '../../10_images/étoile_grise.png'
 import EtoileRouge from '../../10_images/étoile_rouge.png'
+import foto from '../../10_images/logo_kasa.png'
+
 
 //v- fonction
 function DessusCollapse(props) {
@@ -18,8 +20,10 @@ function DessusCollapse(props) {
 
   //v- variable divers
 
-  // "name": "Nathalie Jean",
-  // "picture": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-12.jpg"
+  const HostName = InfoLogement.host.name
+  const Hostpicture = <img className="dessus-photo" src={InfoLogement.host.picture} alt='' />
+
+
 
   let Tags = ''
   Tags = InfoLogement.tags
@@ -27,7 +31,6 @@ function DessusCollapse(props) {
 
   const NbrEtoile = InfoLogement.rating
   let i = 0
-
 
   const etoilegrise = <img className="dessus-etoile" src={EtoileGrise}
     alt='étoile' />
@@ -38,40 +41,34 @@ function DessusCollapse(props) {
   let listStar = [etoilegrise, etoilegrise, etoilegrise, etoilegrise, etoilegrise]
 
   for (i; i < NbrEtoile; i++) {
-
     listStar[i] = etoilerouge
   }
-
-
 
   return (
     <>
       <div className="dessus-corps">
-        <h1 className="dessus-title">{InfoLogement.title} </h1>
-        <h5 className="dessus-location">{InfoLogement.location} </h5>
 
-        <div className='dessus-total-tag'>
-          {listTags}
+        <div className="dessus-texte-tag">
+          <h1 className="dessus-title">{InfoLogement.title} </h1>
+          <h5 className="dessus-location">{InfoLogement.location} </h5>
+
+          <div className='dessus-total-tag'>
+            {listTags}
+          </div>
         </div>
 
-        <div>
-
+        <div className='dessus-nom-photo'>
           <div>
-            {/* <img className="dessus-foto" src={InfoLogement.host.picture} alt='Photo de personne' /> */}
+            {HostName}
+            {Hostpicture}
           </div>
 
-
           <div>
-            {/* {nbrEtoiles} */}
             {listStar[0]}
             {listStar[1]}
             {listStar[2]}
             {listStar[3]}
             {listStar[4]}
-
-
-            {/* <img className="dessus-etoile" src={EtoileRouge}
-              alt='étoile' /> */}
           </div>
         </div>
 
